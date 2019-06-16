89085890aab54a423d16efd0413725a1fe952350
David Obodo <obododavid5@gmail.com>
2019-06-16 17:15:05 +0100

    some odd changes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 
 class Dashboard extends Component {
@@ -34,4 +36,10 @@ const mapStateToProps = (state) => {
 
 
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+	connect(mapStateToProps),
+	firestoreConnect([
+	{collection: 'projects'}
+		])
+	
+ )(Dashboard)
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 import { combineReducers } from 'redux'
+import { firestoreReducer } from 'redux-firestore'
 
 
 const rootReducer = combineReducers({
 	auth: authReducer,
 	project: projectReducer,
+	firestore: firestoreReducer
 });
 
 export default rootReducer
