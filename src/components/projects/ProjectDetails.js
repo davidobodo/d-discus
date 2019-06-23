7ff05557d90dadef105876b7d7d756340b58180c
David Obodo <obododavid5@gmail.com>
2019-06-23 15:47:07 +0100

    deploying of first function to firebase complete

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 
 const ProjectDetails = (props) =>{
@@ -20,7 +21,7 @@ const ProjectDetails = (props) =>{
 	        </div>
 	        <div className="card-action grey lighten-4 grey-text">
 	          <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-	          <div>2nd September, 2am</div>
+	          <div>{moment(project.createdAt.toDate().toString()).calendar()}</div>
 	        </div>
 	      </div>
 	    </div>
