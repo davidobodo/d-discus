7a2b0eff6e2d2c62c7b441ea940b0d17c332c5c9
David Obodo <obododavid5@gmail.com>
2019-06-26 07:47:19 +0100

    deployment of app to firebase successfully

---
+++ dist/precache-manifest.8fa4c9ea6ea10fd80e8969153f891504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dafd2e9db96c01f8006d317a715fd37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db429dc43916c4047c2",
+    "url": "/static/css/main.45a46c71.chunk.css"
+  },
+  {
+    "revision": "98631bdc6906b4f65cc5",
+    "url": "/static/js/2.09ef9c81.chunk.js"
+  },
+  {
+    "revision": "7db429dc43916c4047c2",
+    "url": "/static/js/main.0dba2adb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
